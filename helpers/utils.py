4c0edde1e331c4fbc7c21d0574ac2feca22ce65b
e01627753 <b-weber@gmx.at>
2023-01-01 22:31:12 +0100

    fixed project requirements + fiftyone download + first self-CNN implementation
    still to be done: 1) fix data format for transfer learning 2) integrate image augmentation algorithm 3) finetune self implemented CNN for better comparability ?4) integrate quantization? 5) finish evaluation

--- helpers/utils.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
-
-def plot_img(path="data/train/data/00009e5b390986a0.jpg"):
-
-    fig = plt.figure()
-    plt.imshow(cv2.imread(path))
-    plt.plot()
