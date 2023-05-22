06ae69e8929cf47522d60078a6f48273a3ec9935
e01627753 <b-weber@gmx.at>
2023-05-22 16:41:34 +0200

    optimized models and fixed execute_models.py

--- execute_models.py
+++ execute_models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 ########################################################################
 ######### download, install and import required libraries
 ########################################################################
@@ -6,8 +8,8 @@ import subprocess
 import sys
 import os
 
-libs_to_check = ["argparse", "requests", "numpy", "matplotlib", "time", "fiftyone", "tensorflow"]
 libs_to_install = []
+libs_to_check = ["argparse", "requests", "numpy", "time", "PIL", "tensorflow"]
 
 for lib in libs_to_check:
     try:
@@ -16,28 +18,29 @@ for lib in libs_to_check:
         libs_to_install.append(lib)
 
 if libs_to_install:
-    print(f"INFO: Downloading and installing libraries: {', '.join(libs_to_install)}")
+    print("INFO: Downloading and installing libraries: " + ', '.join(libs_to_install))
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", *libs_to_install])
+        subprocess.check_call(["pip", "install"] + libs_to_install)
     except Exception as e:
-        print(f"ERROR: Could not install following libraries: {e}")
+        print("ERROR: Could not install following libraries: ", str(e))
+        print("INFO: Consider installing the libs manually by executing following command: pip3 install <package-name>")
         sys.exit(1)
         
 # import libraries
 import argparse
 import requests
 import numpy as np
-import matplotlib.pyplot as plt
 import time
-import fiftyone
+from PIL import Image
 import tensorflow as tf
 from tensorflow.keras.utils import array_to_img, img_to_array, load_img
 
 ########################################################################
 ######### define ArgumentParser, add -b option and parse arguments
-########################################################################5
+########################################################################
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", type=int, help="A numeric option")
+parser.add_argument("-m", type=str, help="Ein String als Option")
 args = parser.parse_args()
 
 print()
@@ -49,96 +52,57 @@ elif args.b != 8 and args.b != 16 and args.b != 32:
 else:
     bit = args.b
     
-########################################################################
-######### download models if not already done
-########################################################################
-''' TODO
-MODELS_DIR = os.path.join(os.getcwd(), 'models')
-if not os.path.exists(MODELS_DIR):
-    os.makedirs(MODELS_DIR)
-    
-#STEPS: check if model is already there -> if not download model
-url = "https://raw.githubusercontent.com/user/repo/branch/path/to/file.py"
-#https://github.com/e01627753/bachelor-thesis-transfer-learning.git/models
-response = requests.get(url)
-
-# Überprüfen Sie den Statuscode der Antwort, um sicherzustellen, dass die Anfrage erfolgreich war
-if response.status_code == 200:
-    # Speichern Sie die Datei im aktuellen Arbeitsverzeichnis oder einem anderen Pfad
-    with open("models/file.py", "wb") as file:
-        file.write(response.content)
+if not args.m:
+    sys.exit("ERROR: You have to set -m as an option to clarify the model name of the model you want to execute! Make sure that this name matches the name of the corresponding model in the github repository /models/<your model name> as well as the bit of option -b.")
 else:
-    # Behandeln Sie den Fehler, wenn die Anfrage fehlschlägt
-    print(f"ERROR: could not download {model_name}")
-
-print(f"INFO: loading {bit} bit model...")
-'''
+    model_name = args.m
+    
 ########################################################################
-######### download test data and reformat data
+######### load and reformat test data
 ########################################################################
-DATA_DIR = os.path.join(os.getcwd(), 'data')
-if not os.path.exists(DATA_DIR):
-    os.makedirs(DATA_DIR)
-
-max_samples=50
-
-# delete previously loaded datasets
-for ds_name in fiftyone.list_datasets():
-    ds = fiftyone.load_dataset(ds_name)
-    ds.delete()
-
-# load datasets and merge them
-input_data = fiftyone.zoo.load_zoo_dataset(
-    "open-images-v6",
-    dataset_name="open-images-v6-inference-test",
-    split="test",
-    label_types="classifications",
-    classes=["Vehicle registration plate"],
-    max_samples=max_samples,
-    dataset_dir=DATA_DIR,
-    only_matching=True
-)
-dataset_2 = fiftyone.zoo.load_zoo_dataset(
-    "open-images-v6",
-    dataset_name="dataset_2",
-    split="test",
-    label_types="classifications",
-    classes=["Ball", "Coin", "Fox"],
-    max_samples=max_samples,
-    dataset_dir=DATA_DIR,
-    only_matching=True
-)
-input_data.add_samples(dataset_2)
-dataset_2.delete()
-
-# reformat downloaded data
 IMG_DIM = (300, 300) # format should match input details of models
-X_input = np.array([img_to_array(load_img(img.filepath, target_size=IMG_DIM)) for img in input_data])
-X_input = X_input.astype('float32')
-X_input /= 255
 
-print(X_input.shape)
+image_files = os.listdir("data")  # list of image names in /data
+num_images = len(image_files)
+
+# initialize empty numpy array
+X_input = np.empty((num_images, IMG_DIM[0], IMG_DIM[1], 3), dtype=np.float32)
+
+for i, image_file in enumerate(image_files):
+    # path to image
+    image_path = os.path.join("data", image_file)
+    
+    # open and scale image
+    image = Image.open(image_path)    
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(IMG_DIM)
+
+    # add image to numpy array
+    X_input[i] = np.array(image)
+
+print("X_input.shape:", X_input.shape)
 
 ########################################################################
 ######### load transfer learning models
 ########################################################################
 if (bit == 8): # 8bit int model
-    print(f"INFO: Loading 8 bit model...")
-    interpr = tf.lite.Interpreter(model_path="models/tl_model_8int.tflite")
+    print("INFO: Loading 8 bit model:", model_name)
+    interpr = tf.lite.Interpreter(model_path="models/" + model_name)
     interpr.allocate_tensors()
     input_details = interpr.get_input_details()
     output_details = interpr.get_output_details()
     
 elif (bit == 16): # 16bit float model
-    print(f"INFO: Loading 16 bit model...")
-    interpr = tf.lite.Interpreter(model_path="models/tl_model_16f.tflite")
+    print("INFO: Loading 16 bit model:", model_name)
+    interpr = tf.lite.Interpreter(model_path="models/" + model_name)
     interpr.allocate_tensors()
     input_details = interpr.get_input_details()
     output_details = interpr.get_output_details()
     
 else: # 32bit float model
-    print(f"INFO: Loading 32 bit model...")
-    interpr = tf.lite.Interpreter(model_path="models/tl_model_32f.tflite")
+    print("INFO: Loading 32 bit model:", model_name)
+    interpr = tf.lite.Interpreter(model_path="models/" + model_name)
     interpr.allocate_tensors()
     input_details = interpr.get_input_details()
     output_details = interpr.get_output_details()
@@ -182,26 +146,14 @@ for i in range(X_inf_test.shape[0]):
     if (bit == 8): # rescale output data
         output_scale, output_zero_point = output_details[0]['quantization']
 
-        print(f"INFO: Raw inference output scores: {output}")
-        print(f"INFO: Output scale: {output_scale}")
-        print(f"INFO: Output zero point: {output_zero_point}")
+        print("INFO: Raw inference output scores: ", output)
+        print("INFO: Output scale: ", output_scale)
+        print("INFO: Output zero point: ", output_zero_point)
         
         output_lst[i] = output_scale * (output.astype(np.float32) - output_zero_point)
 
 avg_inf_speed = duration / X_inf_test.shape[0]
 
-print(f"INFO: Inference output: {output_lst[0]}")
-print(f"INFO: Duration in seconds: {duration}")
-print(f"INFO: Average inference speed of {bit} bit model: {avg_inf_speed} seconds per image")
-
-########################################################################
-######### for DEBUGGING purpose only
-########################################################################
-labels = np.array([
-    any(classification['label'] == 'Vehicle registration plate'
-        for classification in sample['positive_labels']['classifications'])
-    for sample in input_data])
-
-y_input = labels.astype(int)
-print("First 10 labels of input_data: ", y_input[:20])
-#check accuracy -> [0, 1]?!
+print("INFO: Inference output: ", output_lst[0])
+print("INFO: Duration in seconds: ", duration)
+print("INFO: Average inference speed of ", bit, " bit model: ", avg_inf_speed, " seconds per image")
